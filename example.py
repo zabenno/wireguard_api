8ad315486e582c4e0ffd5420f2b1a572f6f823ba
zabenno <benc853@gmail.com>
2020-07-13 11:27:03 +1200

    Update example file to work with new method.

--- example.py
+++ example.py
@@ -1,51 +1,36 @@
-from wireguard_servers import wireguard_servers
-from wireguard_clients import wireguard_clients
-from wireguard_database import Wireguard_database
-from wireguard_ipam import wireguard_ipam
-import ipaddress
-
+from wireguard_db import Wireguard_database
 
 test = Wireguard_database()
-test_servers = wireguard_servers(test.db_connection, test.cursor)
-test_clients = wireguard_clients(test.db_connection, test.cursor)
-test_subnets = wireguard_ipam(test.db_connection, test.cursor)
 
-print(test_servers.list_all_servers())
-#Create two servers.
-test_servers.create_wg_server("Wireguard01", "XXXXyyyXXXX", "192.168.0.5", "10.10.10.1")
-test_servers.create_wg_server("Wireguard02", "XXXXZZyyyXXXX", "192.168.0.51", "10.10.11.1")
+#Create servers
+test.create_server("wireguard01", "192.168.2.0", 24, "SSHHUUBBWW", "192.168.2.55", 5128, 20)
+test.create_server("wireguard02", "192.168.1.0", 24, "SSHHUUBfBWW", "192.168.2.55", 5128, 20)
 
-print(test_servers.list_all_servers())
+#Create client to show deletion works
+test.create_client("testclient1", "wireguard02", "JJIINNPkhPSS")
+test.assign_lease("testclient1", "wireguard02")
 
-#Create two thin clients and assign them to both servers.
-test_clients.create_wg_client("client11", "Wireguard02", "ZZZZycyy2XXXX")
-test_clients.create_wg_client("client11", "Wireguard01", "ZZZZycsyy2XXXX")
-test_clients.create_wg_client("client12", "Wireguard01", "ZZZZy2csyy2XXXX")
-test_clients.create_wg_client("client12", "Wireguard02", "ZZZZy2csvyy2XXXX")
+print(test.list_clients())
 
-print(test_clients.list_all_clients())
+#Delete client
+test.delete_client("testclient1")
 
-#Create subnets for both servers.
-test_subnets.create_subnet("10.10.10.0/24", 20)
-test_subnets.create_subnet("10.10.11.0/24", 20)
+print(test.list_clients())
 
-print(test_subnets.list_all_subnets())
+#Create clients
+test.create_client("testclient2", "wireguard01", "JJIIYYNNPPSS")
+test.assign_lease("testclient2", "wireguard01")
+test.create_client("testclient3", "wireguard01", "JJIIYYNNPfPSS")
+test.assign_lease("testclient3", "wireguard01")
 
-#Assign subnets to the servers.
-test_subnets.assign_subnet("10.10.10.0/24", "Wireguard01")
-test_subnets.assign_subnet("10.10.11.0/24", "Wireguard02")
+#Recreate client to show it works and that software is server specific
+test.create_client("testclient1", "wireguard02", "JJIINNPkhPSS")
+test.assign_lease("testclient1", "wireguard02")
 
-print(test_subnets.list_all_subnets())
+print(test.list_clients())
 
-#Create leases for clients.
-test_subnets.create_lease("client12", "Wireguard01")
-test_subnets.create_lease("client12", "Wireguard02")
-test_subnets.create_lease("client11", "Wireguard01")
-test_subnets.create_lease("client11", "Wireguard02")
+#Show retrieving client config works
+test.get_client_config("testclient3", "wireguard01")
 
-print(test_subnets.list_assigned_ips("10.10.10.0/24"))
-print(test_subnets.list_assigned_ips("10.10.10.0/24"))
-
-#Print out required clients info for each server.
-print(test_servers.retrieve_servers_client_details("Wireguard02"))
-print(test_servers.retrieve_servers_client_details("Wireguard02"))
+#Show retrieving server config works
+test.get_server_config("wireguard01")
